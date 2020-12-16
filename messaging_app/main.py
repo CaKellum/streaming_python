b4c32eba4a9d64b36217a4bf1a673143834ed786
CaKellum <cakellum@bsu.edu>
2020-12-16 16:29:26 -0500

    Clean main function in main.py

--- messaging_app/main.py
+++ messaging_app/main.py
@@ -1,23 +1,28 @@
 from os import system as sys
 
+
 def _write_to_file(msg):
-    with open('message.txt','w') as write_file:
+    with open('message.txt', 'w') as write_file:
         write_file.write(msg)
     return write_file
 
 
-def main():
-    print("welcome to the encrypted mesaging.\n please enter the message")
-    message = input("message: ")
-    key = input("password: ")
-    recipient_email = input("email of recipient")
+def _encrypt_message(msg, key):
     file = _write_to_file(message)
     sys_string = 'python ..\my_codex\k_obscurus.py --encrypt {} {}'.format(key, file.name)
     msg = sys(sys_string)
     if msg == 0:
-        print("success")
+        print('success')
     else:
-        print("error try again")
+        print('error try again')
+
+
+def main():
+    print('welcome to the encrypted mesaging.\n please enter the message')
+    msg = input('message: ')
+    key = input('password: ')
+    _encrypt_message(msg, key)
+    recipient_email = input('email of recipient: ')
 
 
 if __name__ == '__main__':
