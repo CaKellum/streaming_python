ccb6032ffe98d8318307ee58f9e9c128bb27dc81
CaKellum <cakellum@bsu.edu>
2020-12-16 18:09:08 -0500

    Add ability to atach message_out.json to email

--- messaging_app/main.py
+++ messaging_app/main.py
@@ -1,5 +1,14 @@
 from os import system as sys
 from termcolor import colored
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import email
+import secrets
+import smtplib
+import ssl
+import json
 
 
 def _write_to_file(msg):
@@ -18,12 +27,37 @@ def _encrypt_message(msg, key):
         print(colored('error try again','red'))
 
 
+def _send_email(recipient_email):
+    msg = MIMEMultipart()
+    msg["From"]= secrets.email
+    msg["To"]=recipient_email
+    msg["Sunbject"]="Super Secret message"
+    msg.attach(MIMEText("secret message sent to you","plain"))
+    attch_file = "message_out.json"
+    with open(attch_file, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {attch_file}",
+    )
+    msg.attach(part)
+    text = msg.as_string()
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(secrets.email, secrets.password)
+        server.sendmail(secrets.email, recipient_email, text)
+    print(colored('success','green'))
+
+
 def main():
     print('welcome to the encrypted mesaging.\n please enter the message')
     msg = input('message: ')
     key = input('password: ')
     _encrypt_message(msg, key)
     recipient_email = input('email of recipient: ')
+    _send_email(recipient_email)
 
 
 if __name__ == '__main__':
